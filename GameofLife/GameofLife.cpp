5c9c032a35bea547ce1c57dd2441ba2cb47190c3
Stefan-23 <sdrmanic@gmail.com>
2023-03-22 09:57:53 +0100

    V2 Game of Life

--- GameofLife/GameofLife.cpp
+++ GameofLife/GameofLife.cpp
@@ -1,210 +1,311 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <vector>
+
 
 using namespace std;
 
-//Copies one array to another.
-void copyArr(int array1[52][102], int array2[52][102])
+const int GRID_SIZE = 40;
+
+void firstInit(vector<vector<bool> >&);
+void secondInit(vector<vector<bool> >&);
+void thirdInit(vector<vector<bool> >&);
+void fourthInit(vector<vector<bool> >&);
+
+void printGrid(vector<vector<bool> >&);
+int getNeighbours(int, int, vector<vector<bool> >&);
+int getState(bool, int);
+
+
+int showMenu()
 {
-	for (int j = 0; j < 52; j++)
-	{
-		for (int i = 0; i < 102; i++)
-			array2[j][i] = array1[j][i];
-	}
-}
-
-//Life function --- Move from stack to heap ---
-void life(int array[52][102], char choice)
-{
-	
-	int temp[52][102]; //Make this preprocessor
-	copyArr(array, temp);
-	for (int j = 1; j < 51; j++)
-	{
-		for (int i = 1; i < 101; i++)
-		{
-			if (choice == 'r')
-			{
-				
-				int count = 0;
-				count = array[j - 1][i] +
-						array[j - 1][i - 1] +
-						array[j][i - 1] +
-						array[j + 1][i - 1] +
-						array[j + 1][i] +
-						array[j + 1][i + 1] +
-						array[j][i + 1] +
-						array[j - 1][i + 1];
-				//The cell dies.
-				if (count < 2 || count > 3)
-					temp[j][i] = 0;
-				//The cell stays the same.
-				if (count == 2)
-					temp[j][i] = array[j][i];
-				//The cell either stays alive, or is "born".
-				if (count == 3)
-					temp[j][i] = 1;
-			}
-
-			else if (choice == 'l')
-			{
-			
-				
-				int count = 0;
-				count = array[j - 1][i] +
-					array[j][i - 1] +
-					array[j + 1][i] +
-					array[j][i + 1];
-				//The cell dies.  
-				if (count < 2 || count > 3)
-					temp[j][i] = 0;
-				//The cell stays the same.
-				if (count == 2)
-					temp[j][i] = array[j][i];
-				//The cell either stays alive, or is "born".
-				if (count == 3)
-					temp[j][i] = 1;
-			}
-		}
-	}
-	//Copies the completed temp array back to the main array. Investigate this
-	copyArr(temp, array);
-}
 
 
-bool compare(int array1[52][102], int array2[52][102])
-{
-	int count = 0;
-	for (int j = 0; j < 52; j++)
-	{
-		for (int i = 0; i < 102; i++)
-		{
-			if (array1[j][i] == array2[j][i])
-				count++;
-		}
-	}
-	
-	//the dimensions of the array multiplied together.
-	if (count == 52 * 102)
-		return true;
-	else
-		return false;
-}
 
-//. A live cell is marked by a '*', and a dead or vacant cell by a '#'.
-void print(int array[52][102])
-{
-	for (int j = 1; j < 51; j++)
-	{
-		for (int i = 1; i < 101; i++)
-		{
-			if (array[j][i] == 1)
-				cout << '*';
-			else
-				cout << ' ';
-		}
-		cout << endl;
-	}
+    cout << "\nPlease chooes which seed pattern to start from:" << endl;
+    cout << "    1. Square " << endl;
+    cout << "    2. Blinker " << endl;
+    cout << "    3. Octagon " << endl;
+    cout << "    4. Glider" << endl;
+
+    cout << endl;
+
+    int choice = 1;
+    cin >> choice;
+
+    return choice;
 }
 
 int main()
-{
-	int gen0[52][102];
-	int todo[52][102];
-	int backup[52][102];
-	char neighborhood;
-	char again;
-	char cont;
-	bool comparison;
-	string decoration;
 
-	//Instructions
-	cout 
-		<< endl << "The rules of the \"Game of Life\" are as follows:" << endl
-		<< endl << "1. Any live cell with fewer than two live neighbors dies, as if caused by under-population."
-		<< endl << "2. Any live cell with two or three live neighbors lives on to the next generation."
-		<< endl << "3. Any live cell with more than three live neighbors dies, as if by overcrowding."
-		<< endl << "4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction." << endl
-		<< endl << "The initial configuration (Generation 0) of the board is determined randomly."
-		<< endl << "Every hundred Generations you will get the option to end or continue the simulation."
-		<< endl << "If a system becomes \"stable\" (meaning the system does not change from one"
-		<< endl << "generation to the next), the simulation will end automatically." << endl << endl;
-	//Continue.
-	do
-	{
-		//Loop to check for proper inputs.
-		do
-		{
-			cout << "Which way would you like to use (r or l): ";
-			cin >> neighborhood;
-		} while (neighborhood != 'r' && neighborhood != 'l');
-		
-		//gives me a warning
-		system("clear");
-		int i = 0;
-		
-		do
-		{
-			//Generates random state of the game board.
-			srand(time(NULL));
-			//array is 102 x 52
-			for (int j = 1; j < 51; j++)
-			{
-				for (int i = 1; i < 101; i++)
-					gen0[j][i] = rand() % 2;
-			}
-			//How big the decoration should be.
-			if (i < 10)
-				decoration = "#############";
-			else if (i >= 10 && i < 100)
-				decoration = "##############";
-			else if (i >= 100 && i < 1000)
-				decoration = "###############";
-			else if (i >= 1000 && i < 10000)
-				decoration = "################";
-			else
-				decoration = "#################";
-			//Prints the generation.
-			cout << decoration << endl << "Generation " << i
-				<< ":" << endl << decoration << endl << endl;
-			//Initializes the arrays by copying the gen0 array to the todo array.
-			if (i == 0)
-				copyArr(gen0, todo);
-			copyArr(todo, backup);
-			print(todo);
-			life(todo, neighborhood);
-			i++;
-			//Pauses the system for 1/10 of a second in order to give the screen time to refresh.
-			system("sleep .1"); // ----  Do this using delta time ----
-			//Checks whether the generation is a multiple of 100 to ask the user if they want to continue the simulation. 
-			if (i % 100 == 1 && i != 1)
-			{
-				cout << endl;
-				//Loop to check for proper inputs.
-				do
-				{
-					cout << "Would you like to continue this simulation? (y/n): ";
-					cin >> cont;
-				} while (cont != 'y' && cont != 'n');
-				if (cont == 'n')
-					break;
-			}
-			//Compares the current generation with a backup generation.
-			comparison = compare(todo, backup);
-			if (comparison == false)
-				system("clear"); //gives me a warning ---- Find a better way ---- 
-			if (comparison == true)
-				cout << endl;
-		} while (comparison == false);
-		//Loop to check for proper inputs.
-		do
-		{
-			cout << "Would you like to run another simulation? (y/n): ";
-			cin >> again;
-		} while (again != 'y' && again != 'n');
-	} while (again == 'y');
-	return 0;
+{
+    cout << "Starting Game Of Life " << endl;
+
+    int pattern = showMenu();
+
+    int numberGenerations = 10;
+    cout << "Please Enter the number of generations:" << endl;
+    cin >> numberGenerations;
+
+    // second array using vectors
+    vector<vector<bool> > theGrid(GRID_SIZE, vector<bool>(GRID_SIZE, 0));       
+    vector<vector<bool> > tmpGrid(GRID_SIZE, vector<bool>(GRID_SIZE, 0));       
+
+    switch (pattern)
+    {
+    case 1: firstInit(theGrid);
+        break;
+
+    case 2: secondInit(theGrid);
+        break;
+
+    case 3: thirdInit(theGrid);
+        break;
+
+    case 4: fourthInit(theGrid);
+        break;
+
+    default:firstInit
+           (theGrid);
+        break;
+    }
+
+    printGrid(theGrid);
+    cout << "Simulation starting in soon" << endl;
+    
+
+    for (int x = 0; x < numberGenerations; x++)                   // Begin Game
+    {
+        //For each cell
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+
+                int local_n = getNeighbours(i, j, theGrid);         
+                bool survival = getState(theGrid[i][j], local_n);   
+                tmpGrid[i][j] = survival;                           
+
+            }
+        }
+        printGrid(tmpGrid);                                         
+        theGrid = tmpGrid;                                         
+
+                                               
+    }
+
+    return 0;
+}
+
+void firstInit(vector<vector<bool> >& grid)
+{
+    grid[1][1] = 1;
+    grid[1][2] = 1;
+    grid[2][1] = 1;
+    grid[2][2] = 1;
+
+    cout << "Set one." << endl;
+}
+
+void secondInit(vector<vector<bool> >& grid)
+{
+
+    grid[1][2] = 1;
+    grid[2][2] = 1;
+    grid[3][2] = 1;
+
+    cout << "Set two ." << endl;
+}
+
+void thirdInit(vector<vector<bool> >& grid)
+{
+
+    grid[1][2] = 1;
+    grid[1][5] = 1;
+
+    grid[2][1] = 1;
+    grid[2][3] = 1;
+    grid[2][4] = 1;
+    grid[2][6] = 1;
+
+    grid[3][2] = 1;
+    grid[3][5] = 1;
+
+    grid[4][2] = 1;
+    grid[4][5] = 1;
+
+    grid[5][1] = 1;
+    grid[5][3] = 1;
+    grid[5][4] = 1;
+    grid[5][6] = 1;
+
+    grid[6][2] = 1;
+    grid[6][5] = 1;
+
+    cout << "Set Three ." << endl;
+
+}
+
+
+void fourthInit(vector<vector<bool> >& grid)
+{
+
+    // NEEDS GRID SIZE OF 40
+    grid[5][1] = 1;
+    grid[5][2] = 1;
+    grid[6][1] = 1;
+    grid[6][2] = 1;
+
+    grid[5][11] = 1;
+    grid[6][11] = 1;
+    grid[7][11] = 1;
+
+    grid[4][12] = 1;
+    grid[8][12] = 1;
+
+    grid[3][13] = 1;
+    grid[9][13] = 1;
+
+    grid[3][14] = 1;
+    grid[9][14] = 1;
+
+    grid[6][15] = 1;
+
+    grid[4][16] = 1;
+    grid[8][16] = 1;
+
+    grid[5][17] = 1;
+    grid[6][17] = 1;
+    grid[7][17] = 1;
+
+    grid[6][18] = 1;
+
+    grid[3][21] = 1;
+    grid[4][21] = 1;
+    grid[5][21] = 1;
+
+    grid[3][22] = 1;
+    grid[4][22] = 1;
+    grid[5][22] = 1;
+
+    grid[2][23] = 1;
+    grid[6][23] = 1;
+
+    grid[1][25] = 1;
+    grid[2][25] = 1;
+    grid[6][25] = 1;
+    grid[7][25] = 1;
+
+    grid[3][35] = 1;
+    grid[4][35] = 1;
+    grid[3][36] = 1;
+    grid[4][36] = 1;
+
+    cout << "Set four." << endl;
+
+}
+
+
+void printGrid(vector<vector<bool> >& grid)
+{
+    cout << endl;
+
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            if (grid[i][j])
+            {
+                cout << " x ";
+            }
+            else
+            {
+                cout << "   ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int getNeighbours(int i, int j, vector<vector<bool> >& grid)
+{
+    int count = 0;
+
+    if ((i > 0) && (j > 0))                         //  Left
+    {
+        if (grid[i - 1][j - 1]) {
+            count++;
+        }
+    }
+
+    if (i > 0)                                      //  Mid
+    {
+        if (grid[i - 1][j]) {
+            count++;
+        }
+    }
+
+    if ((i > 0) && (j < GRID_SIZE - 1))               //  Right
+    {
+        if (grid[i - 1][j + 1]) {
+            count++;
+        }
+    }
+
+    if ((j > 0))                                    // Mid Left
+    {
+        if (grid[i][j - 1]) {
+            count++;
+        }
+    }
+
+    if (j < GRID_SIZE - 1)                            // Mid Right
+    {
+        if (grid[i][j + 1]) {
+            count++;
+        }
+    }
+
+    if ((j > 0) && (i < GRID_SIZE - 1))               // Bottom Left
+    {
+        if (grid[i + 1][j - 1]) {
+            count++;
+        }
+    }
+
+    if (i < GRID_SIZE - 1)                            // Bottom Mid
+    {
+        if (grid[i + 1][j]) {
+            count++;
+        }
+    }
+
+    if ((i < GRID_SIZE - 1) && (j < GRID_SIZE - 1))     // Bottom Right
+    {
+        if (grid[i + 1][j + 1]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int getState(bool current_state, int n)
+{
+    if (current_state && (n < 2)) {
+        return 0;
+    }
+    else if (current_state && (n == 2 || n == 3))
+    {
+        return 1;
+    }
+    else if (current_state && (n > 3))
+    {
+        return 0;
+    }
+    else if ((current_state == 0) && (n == 3))
+    {
+        return 1;
+    }
+
+    return current_state;
 }
